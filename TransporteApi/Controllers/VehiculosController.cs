a52da65fc39bbbc4e2de15473a482e7dc93713d9
Emiliano Alvarez <MetalEmi7@gmail.com>
2025-07-09 00:46:32 -0300

    Nuevos endpoints con ejemplos basicos utilizando dbcontext y linq

--- TransporteApi/Controllers/VehiculosController.cs
+++ TransporteApi/Controllers/VehiculosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TransporteAPI.Data;
 using TransporteAPI.Models;
@@ -9,7 +10,6 @@ namespace TransporteApi.Controllers
     [Controller]
     public class VehiculosController : Controller
     {
-
         private readonly AppDbContext _context;
 
         public VehiculosController(AppDbContext context)
@@ -60,13 +60,87 @@ namespace TransporteApi.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// 1. Hacemnos el cotejamiento dentro del DbContext
+        /// </summary>
+        /// <param name="brand"></param>
+        /// <returns></returns>
+        [HttpGet("GetVehicleByBrand")]
+        public async Task<ActionResult> GetVehicleByBrand (string brand)
+        {
+            try
+            {
+                var Results = await _context.Vehiculos.Where(x => x.Marca == brand).ToListAsync();
+                return Ok(Results);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+                return BadRequest(ex);
+            }
+        }
+
+        /// <summary>
+        /// 2. Buscar por palabra contenida (Vehiculos.Modelo)
+        /// </summary>
+        /// <param name="palabra"></param>
+        /// <returns></returns>
+        [HttpGet("BuscarPorPalabraContenida_SobreModelo")]
+        public async Task<ActionResult> BuscarPorPalabraContenida_SobreModelo(string palabra)
+        {
+            try
+            {
+                var Results = await _context.Vehiculos.Where(x => x.Modelo.Contains(palabra)).ToListAsync();
+                return Ok(Results);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+                return BadRequest(ex);
+            }
+        }
+
+        /// <summary>
+        /// 3. Ordenar por año descendente
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("OrdenarPorAnioDescendente")]
+        public async Task<ActionResult> OrdenarPorAnioDescendente()
+        {
+            try
+            {
+                var Results = await _context.Vehiculos.OrderByDescending(x => x.Anio).ToListAsync();
+                return Ok(Results);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+                return BadRequest(ex);
+            }
+        }
 
 
+        /// <summary>
+        /// 4. Seleccionar solo algunas propiedades (proyección)
+        /// </summary>
+        /// <returns> Marca y Modelo </returns>
+        [HttpGet("SeleccionarSoloAlgunasPropiedades")]
+        public async Task<ActionResult> SeleccionarSoloAlgunasPropiedades()
+        {
+            try
+            {
+                var Results = await _context.Vehiculos.Select(x => new { x.Modelo, x.Marca}).ToListAsync();
+                return Ok(Results);
+            }
+            catch (Exception ex)
+            {
 
-
-
-
-
-
+                throw;
+                return BadRequest(ex);
+            }
+        }
     }
 }
